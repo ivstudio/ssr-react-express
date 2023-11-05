18d0e99bfb08f15b5611ab4a46b097a75f95c42e
Iggy Villamar <iggy@ivstudio.com>
2023-11-05 17:40:30 -0500

    Remove style-loader and cleanup package

--- src/components/AppBar/AppBar.js
+++ src/components/AppBar/AppBar.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import Menu from '@/components/Menu';
 import styles from './AppBar.module.scss';
+import { Link } from 'react-router-dom';
 
 const AppBar = () => (
     <header className={styles.container}>
-        <div className={styles.logo}>Logo</div>
+        <Link
+            className={styles.logo}
+            to="/"
+            title="ssr react express boilerplate"
+        >
+            SSR React Express
+        </Link>
         <Menu />
     </header>
 );
--- src/server/renderer.js
+++ src/server/renderer.js
@@ -20,10 +20,13 @@ export default req => {
 
     return `
     <!DOCTYPE html>
-    <html>
+    <html class="no-js" lang="en">
         <head>
           <title>React Redux Express SSR</title>
+          <meta charset="utf-8">
+          <meta name="viewport" content="width=device-width, initial-scale=1">
           <link rel="stylesheet" href="/main.css">
+          <meta name="description" content="ssr react express redux boilerplate">
         </head>
         <body>
           <div id="root">${html}</div>
